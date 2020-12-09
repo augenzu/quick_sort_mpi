5f30c8e5fabdead00802d9a27bb2595c7faf4780
augenzu <elfokd@gmail.com>
2020-12-09 20:39:55 +0300

    Small fix

--- qsort-mpi.cpp
+++ qsort-mpi.cpp
@@ -6,6 +6,12 @@ q_sort(int *data, size_t elm_cnt)
 {
     MPI_Init(NULL, NULL);
 
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    
+    std::cout << "rank " << rank << " of size " << size << std::endl;
+
     std::cout << "Am I here?" << std::endl;
 
     MPI_Finalize();
